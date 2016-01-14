5696d3f5ca26a78eeeed988a8fe34fbfd9dc1604
pranavn-cuelogic <pranav.naxane@cuelogic.co.in>
2016-01-14 18:36:11 +0530

    Completed exercise 34

---
+++ ex34.py
@@ -0,0 +1,14 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal is at 1 %s" % animals[1]
+print "The third(3rd) animal %s" % animals[2]
+print "The first(1st) animal %s" % animals[0]
+print "The animal at 3 %s" % animals[3]
+print "The fifth(5th) animal %s" % animals[4]
+print "The animal at 2 %s" % animals[2]
+print "The sixth(6th) animal %s" % animals[5]
+print "The animal at 4 %s" % animals[4]
+
+# In this exercise we've learned how to access the nth
+# element from the list also the list element starts from
+# the 0th position.
