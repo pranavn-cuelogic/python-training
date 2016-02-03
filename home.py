e62230365302bd2fcc7edb689a26cd54cee145bd
pranavn-cuelogic <pranav.naxane@cuelogic.co.in>
2016-02-03 16:09:17 +0530

    Remove unwanted code from the file

--- home.py
+++ home.py
@@ -26,6 +26,8 @@ def enterEmail():
 
 def loginAndFetchRawData(username, password):
     mail_list = imaplib.IMAP4_SSL("imap.gmail.com")
+
+    # Exception Handling
     try:
         mail_list.login(username, password)
     except Exception:
@@ -48,9 +50,8 @@ def loginAndFetchRawData(username, password):
     print "\nNow enter the start date & end date between which you want to fetch emails."
     from_date = raw_input('Enter start date. e.g: 01-Jan-2016: ')
     to_date = raw_input('Enter end date. e.g: 31-Jan-2016: ')
-    print "\n", "**" * 25, "\nProcessing... please take a sip of coffee.\n", "**" * 25
-    # from_date = "01-Jan-2016"
-    # to_date = "01-Feb-2016"
+    print "\n", "~~" * 25, "\nProcessing... please take a sip of coffee.\n", "~~" * 25
+
     mail_type, data = mail_list.uid(
         'search', None, '(SENTSINCE {from_date} SENTBEFORE {to_date})'.format(
             from_date=from_date, to_date=to_date))
@@ -64,12 +65,12 @@ def loginAndFetchRawData(username, password):
             headers = Parser().parsestr(msg_data)
             mail_data.append(headers)
 
-    # print "Total email Count: ", total_email_count
     target = open('emails.txt', 'a')
     target.write(str(mail_data))
     processRawData(total_email_count, mail_data, folder)
-    mail_list.close()
-    mail_list.logout()
+
+    mail_list.close()  # closing the connection for created mail object
+    mail_list.logout()  # logging out from the account.
     exit(1)
 
 
@@ -80,19 +81,6 @@ def processRawData(total_email_count, user_data, folder):
             user_detail = data['from']
         else:
             user_detail = data['to']
-
-        # name = ''
-        # if user_detail[0] != '':
-        #     name = ''.join(n for n in user_detail[0] if n.isalnum())
-
-        # email = ''
-        # if user_detail[1] != '':
-        #     email = user_detail[1]
-
-        # print "Name==>", name
-        # print "Email==>", email
-        # print "Date==>", data['date']
-        # print "===xx===xx===xx===\n"
 
         if user_detail in individual_email_count:
             individual_email_count[user_detail] = individual_email_count[user_detail] + 1
@@ -106,14 +94,14 @@ def displayResult(individual_email_count, total_email_count):
     i = 1
     print '==' * 40
     print "Total no. of emails: ", total_email_count
-    print '{0:40} {1:40} {2:10}'.format("==>Name<==", "==>Email<==", "==>No of emails (in percentage)<==")
+    print '{0:40} {1:40} {2:40} {3:10}'.format("==>Name<==", "==>Email<==", "==>Email count<==", "==>No of emails (in percentage)<==")
     for name, count in sorted(individual_email_count.items(), key=lambda (k, v):(v, k))[::-1] :
         if i <= 10:
             percentage = (count / float(total_email_count)) * 100
             user_detail = parseaddr(name)
             if user_detail[0] != '':
-                print '{0:40} {1:40} {2:10}'.format(
-                    user_detail[0], user_detail[1], round(percentage, 2)
+                print '{0:40} {1:40} {2:40} {3:10}'.format(
+                    user_detail[0], user_detail[1], count, round(percentage, 2)
                 )
 
         i += 1
