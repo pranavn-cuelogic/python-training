a0d237a48c456d62c056df2d1b87871c28b2e41a
Pranav Naxane <pranav.naxane@cuelogic.co.in>
2016-02-23 13:15:22 +0530

    Added two small project of django 1. Polls & 2.Notes

---
+++ django_project/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pranav/python/django_project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ djangonote_project/djangonote/notes/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from notes.views import index_view, add_note, add_tag, tag_search
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', index_view, name='index'),
+    url(r'^addnote/', add_note, name='addnote'),
+    url(r'^addtag/', add_tag, name='addtag'),
+    url(r'^tags/(?P<slug>[-\w]+)/$', tag_search, name='tagsearch'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'home'}, name='logout'),
+)
